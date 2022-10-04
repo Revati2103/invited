21e22178469c8dcf9d1fb926a9846dcd889bc531
Revati <revati.v.bordawekar@gmail.com>
2022-10-04 09:31:59 -0700

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
+import CreateEvent from './components/CreateEvent';
+import DisplayEvent from './components/DisplayEvent';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+
+<Routes>
+    <Route exact path='/' element={<Home />}>
+       
+    </Route > 
+    <Route path = '/create' element = {<CreateEvent />}>
+      
+    </Route>
+    <Route path = '/display' element = {<DisplayEvent />}>
+      
+    </Route> 
+  
+</Routes>
+
+
+</BrowserRouter>
+
     </div>
   );
 }
